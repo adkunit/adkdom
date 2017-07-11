116d4d2dc5b28d7f541fbfbd5c56d4346b191600
adkunit <adkgadget@gmail.com>
2017-07-11 10:08:38 +0300

    Add files via upload

---
+++ js/jquery.cook.js
@@ -0,0 +1,15 @@
+﻿ var isMobileView = false,
+            needscookie = false,
+            cookievalue = '';
+
+        if (window.location.search.indexOf('desktop=1') != -1) {
+            needscookie = true;
+            cookievalue = 'desktop';
+        } else if (window.location.search.indexOf('mobile=1') != -1) {
+            needscookie = true;
+            cookievalue = 'mobile';
+        }
+        if (needscookie === true) {
+            $.cookie('viewpreference', cookievalue, { path: 'index.html' });
+            window.location = window.location.pathname;
+        }
---
+++ js/tools.js
@@ -0,0 +1,60 @@
+﻿   $(function() {
+    $( "#tabs" ).tabs({
+      event: "mouseover"
+    });
+  });
+  
+  $(function() {
+    $( "#MyTabs" ).tabs({
+      event: "mouseover"
+    });
+  });
+  
+  $(function() {
+    $( "#FotoTabs" ).tabs({
+      event: "mouseover"
+    });
+  });
+  
+  $(function() {
+    $( "#CoastTabs" ).tabs({
+      event: "mouseover"
+    });
+  });
+  
+ 
+ $(function(){
+      $("#accordion").accordion({
+        header: '.head',
+        event: 'mouseover',
+        fillSpace: true,
+        navigation: true
+      });
+    });
+	
+	$(function(){
+      $("#MyAccordion").accordion({
+        header: '.head',
+        event: 'mouseover',
+        fillSpace: true,
+        navigation: true
+      });
+    });
+	
+	$(function(){
+      $("#FotoAccordion").accordion({
+        header: '.head',
+        event: 'mouseover',
+        fillSpace: true,
+        navigation: true
+      });
+    });
+	
+	$(function(){
+      $("#CoastAccordion").accordion({
+        header: '.head',
+        event: 'mouseover',
+        fillSpace: true,
+        navigation: true
+      });
+    });
